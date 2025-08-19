e158aaae4ae8a3a962a8578358ffbeb53c3f619a
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-08-19 14:13:11 +0900

    init

---
+++ src/test/java/com/ai/chatbot/demo/chatbot/ChatbotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.chatbot.demo.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
