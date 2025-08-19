4cf38aebd9bea32cdde807ca130ba2b1a97c52ca
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-08-20 01:23:33 +0900

    feat: Creating a ChatClient - Using Defaults

--- src/main/java/com/ai/chatbot/demo/config/ChatClientConfig.java
+++ src/main/java/com/ai/chatbot/demo/config/ChatClientConfig.java
@@ -9,6 +9,8 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.build();
+        return chatClientBuilder
+                .defaultSystem("사람들이 목표를 입력하면 그걸 달성하기 위한 할 일을 추천하는 챗봇이야")
+                .build();
     }
 }
