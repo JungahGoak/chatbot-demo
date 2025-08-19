cb5fef76b307efedbed29eb6fdb1ccf078ecd63e
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-08-19 15:20:30 +0900

    feat: Creating a ChatClient - Streaming Responses

--- src/main/java/com/ai/chatbot/demo/controller/ChatController.java
+++ src/main/java/com/ai/chatbot/demo/controller/ChatController.java
@@ -5,6 +5,9 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.stream.Collectors;
 
 @RestController
 public class ChatController {
@@ -23,5 +26,16 @@ public class ChatController {
                 .entity(ReferenceGoals.class);
 
         return referenceGoals.toString();
+    }
+
+    @GetMapping("/stream")
+    Flux<String> generationStreaming(@RequestParam String goal) {
+
+        return this.chatClient.prompt()
+                .user(u -> u
+                        .text("{goal}를 달성할 세부 목표 5개 짜줘")
+                        .param("goal", goal))
+                .stream()
+                .content();
     }
 }
