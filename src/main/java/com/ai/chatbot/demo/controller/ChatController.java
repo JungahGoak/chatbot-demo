8e1a5f36f241c73de5973712de0cf8bb8de48262
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-08-20 01:09:41 +0900

    refactor: ChatClient bean

--- src/main/java/com/ai/chatbot/demo/controller/ChatController.java
+++ src/main/java/com/ai/chatbot/demo/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.ai.chatbot.demo.controller;
 
 import com.ai.chatbot.demo.dto.response.ReferenceGoals;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,17 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequiredArgsConstructor
 public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
-    }
-
     @GetMapping("/ai")
     String generation(@RequestParam String input) {
-        ReferenceGoals referenceGoals = this.chatClient.prompt()
+        ReferenceGoals referenceGoals = chatClient.prompt()
                 .user(input)
                 .call()
                 .entity(ReferenceGoals.class);
@@ -29,9 +27,9 @@ public class ChatController {
     @GetMapping("/stream")
     Flux<String> generationStreaming(@RequestParam String goal) {
 
-        return this.chatClient.prompt()
+        return chatClient.prompt()
                 .user(u -> u
-                        .text("{goal}를 달성할 세부 할  5개 짜줘")
+                        .text("{goal}를 달성할 세부 할 일 5개 짜줘")
                         .param("goal", goal))
                 .stream()
                 .content();
