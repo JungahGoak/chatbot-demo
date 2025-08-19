c9b98c5fdfb87d06afab11986681505402e413c5
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-08-19 14:44:41 +0900

    feat: Creating a ChatClient - Returning an Entity

--- src/main/java/com/ai/chatbot/demo/controller/ChatController.java
+++ src/main/java/com/ai/chatbot/demo/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.ai.chatbot.demo.controller;
 
+import com.ai.chatbot.demo.dto.response.ReferenceGoals;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,13 +17,11 @@ public class ChatController {
 
     @GetMapping("/ai")
     String generation(@RequestParam String input) {
-        try {
-            return this.chatClient.prompt()
-                    .user(input)
-                    .call()
-                    .content();
-        } catch (Exception e) {
-            return "오류 발생: " + e.getMessage();
-        }
+        ReferenceGoals referenceGoals = this.chatClient.prompt()
+                .user(input)
+                .call()
+                .entity(ReferenceGoals.class);
+
+        return referenceGoals.toString();
     }
 }
---
+++ src/main/java/com/ai/chatbot/demo/dto/response/ReferenceGoals.java
@@ -0,0 +1,6 @@
+package com.ai.chatbot.demo.dto.response;
+
+import java.util.List;
+
+public record ReferenceGoals(List<String> goals) {
+}
